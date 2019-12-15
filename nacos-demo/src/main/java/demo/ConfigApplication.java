ce7ad53195b1ea514ee1942a45f4e60b402ce06f
lizhs <lizhs@sunline.cn>
2019-12-16 01:19:08 +0800

    x

--- nacos-demo/src/main/java/demo/ConfigApplication.java
+++ nacos-demo/src/main/java/demo/ConfigApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
